702d9bcb8e28b6fdc19893aaaf3f7cd9f3e15e98
hrishikesh <hrishikesh.muley@gmail.com>
2022-03-04 17:35:50 +0530

    commit 2

---
+++ springBootEmployeeEMS/src/main/java/com/employee/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.employee.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class EmployeeController {
+
+	@GetMapping("/index")
+	public String index() {
+		return "index";
+	}
+
+}
